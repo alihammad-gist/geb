1dc8dfca31b7ef278180328f96785767b8c6cc97
Ali Hammad <syedalihammadshah@gmail.com>
2020-07-03 03:58:14 +0500

    pushing website

---
+++ bash/gitlab-deploy.sh
@@ -0,0 +1,7 @@
+gatsby clean &&
+PATH_PREFIX='/web' gatsby build --prefix-paths && 
+echo 'commiting changes in the repository' &&
+git add -A && 
+git commit -m 'pushing website' && 
+echo 'pushing master branch to gitlab server' &&
+git push gitlab master
